635bbbcf05657fa23cfec9586385c4e2778044c8
yashnigamp <yashnigam.p@gmail.com>
2022-11-30 23:00:23 +0530

    Cors Issue

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = require('./Router/routing');
 const bodyparser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyparser.json());
+app.use(cors());
 app.use('/',router);
 app.use(express.static(__dirname + '/Assets/Images'));
 
